43fca4834524c8dd89a9d8a6b6d0445306ce302a
zephyrnaut23 <andreasnotokusumo23@gmail.com>
2020-11-15 19:32:53 +0700

    Login UI Done, Auth 50%

--- lib/ui/authentication/splashscr.dart
+++ lib/ui/authentication/splashscr.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:pack_me/services/loggedDecider.dart';
+import 'package:pack_me/ui/authentication/login.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-      splash: Text('a'),
-      nextScreen: LoginDecider(),
+      splash: Text('a'), //TBA Images
+      nextScreen: Login(),
       splashTransition: SplashTransition.rotationTransition,
     );
   }
